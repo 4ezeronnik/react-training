1ff7bd081af9d6bedf20efaf7c750657ae007126
Andrii Reznichenko <4ezeronnik@gmail.com>
2022-07-17 23:07:28 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,19 +7,42 @@ class App extends Component {
     bad: 0
   }
 
+  addGoodFeedback = () => {
+    this.setState(prevState => ({
+      good: this.state.good + 1,
+    }));
+  };
+
+  addNeutralFeedback = () => {
+    this.setState(prevState => ({
+      neutral: this.state.neutral + 1,
+    }));
+  };
+
+  addBadFeedback = () => {
+    this.setState(prevState => ({
+      bad: this.state.bad + 1,
+    }));
+  };
+
+  countTotalFeedback() {
+    
+  } 
+
+
   render() {
     return (
       <>
         <h2>Please leave feedback</h2>
         <div>
-          <button>Good</button>
-          <button>Neutral</button>
-          <button>Bad</button>
+          <button onClick={this.addGoodFeedback}>Good</button>
+          <button onClick={this.addNeutralFeedback}>Neutral</button>
+          <button onClick={this.addBadFeedback}>Bad</button>
         </div>
         <h3>Statistics</h3>
-        <p>Good:</p>
-        <p>Neutral:</p>
-        <p>Bad:</p>
+        <p>Good: {this.state.good}</p>
+        <p>Neutral: {this.state.neutral}</p>
+        <p>Bad: { this.state.bad}</p>
         
 
 
