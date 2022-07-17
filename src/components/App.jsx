5deb7c64133e588430f780fbd599c87c27cb5ece
Andrii Reznichenko <4ezeronnik@gmail.com>
2022-07-17 20:51:48 +0300

    updated app.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,32 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, {Component} from "react";
+
+class App extends Component {
+  state = {
+    good: 0, 
+    neutral: 0,
+    bad: 0
+  }
+
+  render() {
+    return (
+      <>
+        <h2>Please leave feedback</h2>
+        <div>
+          <button>Good</button>
+          <button>Neutral</button>
+          <button>Bad</button>
+        </div>
+        <h3>Statistics</h3>
+        <p>Good:</p>
+        <p>Neutral:</p>
+        <p>Bad:</p>
+        
+
+
+      </>
+    )
+  }
+}
+
+
+export default App;
