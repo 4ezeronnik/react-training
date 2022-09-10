7c68633d3f9bc0c39a98f4ac5bdc41e97d0fea81
Andrii Reznichenko <4ezeronnik@gmail.com>
2022-09-10 11:56:32 +0300

    added changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,15 @@
-import { Switch, Route } from 'react-router-dom';
-import AppBar from './components/AppBar/AppBar';
-import SignupForm from 'components/SignupForm/SignupForm';
 
+// import SignupForm from 'components/SignupForm/SignupForm';
+import ColorPicker from "./ColorPicker/ColorPicker";
+
+const colorPickerOptions = [
+  { label: 'red', color: '#F44336' },
+  { label: 'green', color: '#4CAF50' },
+  { label: 'blue', color: '#2196F3' },
+  { label: 'grey', color: '#607D8B' },
+  { label: 'pink', color: '#E91E63' },
+  { label: 'indigo', color: '#3F51B5' },
+];
 
 const containerStyles = {
   maxWidth: 1170,
@@ -14,29 +22,9 @@ const containerStyles = {
 export default function App() {
     return (
         <div style={containerStyles}>
-            <AppBar />
-
-            <Switch>
-                <Route path="/signup">
-                    <SignupForm />
-                </Route>
-
-                <Route path="/colorpicker">
-                    <ColorPicker options={colorPickerOptions} />
-                </Route>
-
-                <Route path="/counter">
-                    <Counter />
-                </Route>
-
-                <Route path="/clock">
-                    <Clock />
-                </Route>
-
-                <Route path="/pokemon">
-                    <PokemonView />
-                </Route>
-            </Switch>
+        {/* <SignupForm /> */}
+        <ColorPicker options={colorPickerOptions}/>
+        
         </div>
     );
 }
--- src/components/SignupForm/SignupForm.js
+++ src/components/SignupForm/SignupForm.js
@@ -1,39 +1,87 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import styles from './SignupForm.module.css';
 
-class SignupForm extends Component {
-  state = {
-      email: '',
-      password: '',
-  };
+export default function SignupForm() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  
+  const handleChange = event => {
+    const { name, value } = event.target;
 
-  handleChange = evt => {
-   const { name, value } = evt.target;
-   this.setState({ [name]: value });
-};
+    switch (name) {
+      case 'email':
+        setEmail(value);
+        break;
+      
+      case 'password':
+        setPassword(value);
+        break;
+      
+      default: 
+        return
+    }
+  }
 
-   render() {
-    return (
-       <form className={styles.form} autoComplete="off">
+
+
+  return (
+     <form className={styles.form} autoComplete="off">
         <label className={styles.label}>
          <span>Почта</span>
          <input
            type="email"
             name="email"            
-            onChange={this.handleChange}
-            value={this.state.email}          />
+            onChange={handleChange}
+          value={email}
+        />
       </label>
- <label className={styles.label}>
-   <span>Пароль</span>  <input
+
+      <label className={styles.label}>
+    <span>Пароль</span>  <input
      type="password"
     name="password"
-    onChange={this.handleChange}     
-    value={this.state.password}   />
+    onChange={handleChange}     
+    value={password}   />
  </label>
 
-        <button type="submit">Зарегистрироваться</button>
-      </form>
-    );   }
+       <button type="submit">Зарегистрироваться</button>
+       </form>
+  )
 }
+
+// class oldSignupForm extends Component {
+//   state = {
+//       email: '',
+//       password: '',
+//   };
+
+//   handleChange = evt => {
+//    const { name, value } = evt.target;
+//    this.setState({ [name]: value });
+// };
+
+//    render() {
+//     return (
+//        <form className={styles.form} autoComplete="off">
+//         <label className={styles.label}>
+//          <span>Почта</span>
+//          <input
+//            type="email"
+//             name="email"            
+//             onChange={this.handleChange}
+//             value={this.state.email}          />
+//       </label>
+//  <label className={styles.label}>
+//    <span>Пароль</span>  <input
+//      type="password"
+//     name="password"
+//     onChange={this.handleChange}     
+//     value={this.state.password}   />
+//  </label>
+
+//         <button type="submit">Зарегистрироваться</button>
+//       </form>
+//     );   }
+// }
  
-export default SignupForm;
+// export default SignupForm;
