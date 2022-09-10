7f771dbc9d1d4f1869fd56fbf7daa9bc473b952e
Andrii Reznichenko <4ezeronnik@gmail.com>
2022-09-10 22:56:37 +0300

    added changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,16 @@
 
 // import SignupForm from 'components/SignupForm/SignupForm';
-import ColorPicker from "./ColorPicker/ColorPicker";
+// import ColorPicker from "./ColorPicker/ColorPicker";
+import Counter from './Counter/Counter';
 
-const colorPickerOptions = [
-  { label: 'red', color: '#F44336' },
-  { label: 'green', color: '#4CAF50' },
-  { label: 'blue', color: '#2196F3' },
-  { label: 'grey', color: '#607D8B' },
-  { label: 'pink', color: '#E91E63' },
-  { label: 'indigo', color: '#3F51B5' },
-];
+// const colorPickerOptions = [
+//   { label: 'red', color: '#F44336' },
+//   { label: 'green', color: '#4CAF50' },
+//   { label: 'blue', color: '#2196F3' },
+//   { label: 'grey', color: '#607D8B' },
+//   { label: 'pink', color: '#E91E63' },
+//   { label: 'indigo', color: '#3F51B5' },
+// ];
 
 const containerStyles = {
   maxWidth: 1170,
@@ -23,7 +24,8 @@ export default function App() {
     return (
         <div style={containerStyles}>
         {/* <SignupForm /> */}
-        <ColorPicker options={colorPickerOptions}/>
+        {/* <ColorPicker options={colorPickerOptions}/> */}
+        <Counter />
         
         </div>
     );
